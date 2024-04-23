3f7836f85287b2e439e1a8e95f9e5ce551d4aacc
Randy S. Hunsaker <hunsra@hotmail.com>
2024-04-23 06:44:10 -0700

    Added lifecycle event handler for Windows platform to enable application close cancellation if desired.

--- MountFujiApp/MauiProgram.cs
+++ MountFujiApp/MauiProgram.cs
@@ -21,54 +21,89 @@ using CommunityToolkit.Maui;
 using MetroLog.MicrosoftExtensions;
 using Mopups.Hosting;
 using MountFuji.Extensions;
+using Microsoft.Maui.LifecycleEvents;
+using System.Runtime.CompilerServices;
 
 namespace MountFuji;
 
 [ExcludeFromCodeCoverage]
 public static class MauiProgram
 {
-    public static MauiApp CreateMauiApp()
-    {
-        var builder = MauiApp.CreateBuilder();
-        builder
-            .UseMauiApp<App>()
-            .ConfigureFonts(fonts =>
-            {
-                fonts.AddFont("Roboto-Regular.ttf", "FontRegular");
-                fonts.AddFont("Roboto-Bold.ttf", "FontBold");
-                fonts.AddFont("Roboto-Italic.ttf", "FontItalic");
-                fonts.AddFont("Roboto-Black.ttf", "FontBlack");
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("Roboto-Regular.ttf", "FontRegular");
+				fonts.AddFont("Roboto-Bold.ttf", "FontBold");
+				fonts.AddFont("Roboto-Italic.ttf", "FontItalic");
+				fonts.AddFont("Roboto-Black.ttf", "FontBlack");
 
-                fonts.AddFont("MaterialIconsOutlined-Regular.otf", "MaterialIcons");
-            })
-            .ConfigureMopups()
-            .UseTinyMvvm()
-            .UseMauiCommunityToolkit();
+				fonts.AddFont("MaterialIconsOutlined-Regular.otf", "MaterialIcons");
+			})
+			.ConfigureMopups()
+			.UseTinyMvvm()
+			.UseMauiCommunityToolkit();
 
-        builder.Logging.AddConsoleLogger(_ => { })
-            .AddStreamingFileLogger(options =>
-            {
-                options.RetainDays = 2;
-                options.FolderPath = Path.Combine(FileSystem.AppDataDirectory, "fuji");
-            });
+		builder.Logging.AddConsoleLogger(_ => { })
+			.AddStreamingFileLogger(options =>
+			{
+				options.RetainDays = 2;
+				options.FolderPath = Path.Combine(FileSystem.AppDataDirectory, "fuji");
+			});
 
-        // hopfully this defers the wiring up of application until after the app is initialized
-        builder.Services.AddSingleton<Application>((provider => Application.Current));
-        
-    builder.Services.AddStrategies();
-        builder.Services.AddViewViewModels();
-        builder.Services.AddConfigService();
-        builder.Services.AddServices();
-        
-        MauiApp built =  builder.Build();
-  
- 
-        IPreferencesService preferencesService = built.Services.GetService<IPreferencesService>();
-        preferencesService.Load();
+		// hopfully this defers the wiring up of application until after the app is initialized
+		builder.Services.AddSingleton<Application>((provider => Application.Current));
 
-        ISystemsService systemsService = built.Services.GetService<ISystemsService>();
-        systemsService.Load();
-        
-        return built;
-    }
+		builder.Services.AddStrategies();
+		builder.Services.AddViewViewModels();
+		builder.Services.AddConfigService();
+		builder.Services.AddServices();
+
+#if WINDOWS
+
+		builder.ConfigureLifecycleEvents(events =>
+		{
+			events.AddWindows(lifecycle =>
+			{
+				lifecycle.OnWindowCreated(window =>
+				{
+					var handle = WinRT.Interop.WindowNative.GetWindowHandle(window);
+					var id = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(handle);
+					var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(id);
+					appWindow.Closing += (sender, e) =>
+					{
+						e.Cancel = true;
+					};
+				});
+			});
+		});
+
+#elif MACCATALYST
+
+		builder.ConfigureLifecycleEvents(events =>
+		{
+			events.AddiOS(iOSLifecycleBuilder =>
+			{
+				iOSLifecycleBuilder.WillTerminate((app) =>
+				{
+					app.CancelAllLocalNotifications();
+				});
+			});
+		});
+
+#endif
+		MauiApp built = builder.Build();
+
+
+		IPreferencesService preferencesService = built.Services.GetService<IPreferencesService>();
+		preferencesService.Load();
+
+		ISystemsService systemsService = built.Services.GetService<ISystemsService>();
+		systemsService.Load();
+
+		return built;
+	}
 }
